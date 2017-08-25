bb4f6dd2d1ef0cdb3665750b753f394c728ee906
Courtney Davis <cedavis12@gmail.com>
2017-08-24 23:20:34 -0400

    Added validation rules for the Notes field

--- src/Treehouse.FitnessFrog/Models/Entry.cs
+++ src/Treehouse.FitnessFrog/Models/Entry.cs
@@ -3,14 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Treehouse.FitnessFrog.Models
 {
-    /// <summary>
+
     /// Represents an activity that has been logged in the Fitness Frog app.
-    /// </summary>
     public class Entry
     {
-        /// <summary>
+
         /// The intensity level of the activity.
-        /// </summary>
         public enum IntensityLevel
         {
             Low,
@@ -18,9 +16,8 @@ namespace Treehouse.FitnessFrog.Models
             High
         }
 
-        /// <summary>
+
         /// Default constructor.
-        /// </summary>
         public Entry()
         {
         }
@@ -50,45 +47,38 @@ namespace Treehouse.FitnessFrog.Models
             Notes = notes;
         }
 
-        /// <summary>
+
         /// The ID of the entry.
-        /// </summary>
         public int Id { get; set; }
 
-        /// <summary>
+
         /// The date of the entry. Should not include a time portion.
-        /// </summary>
         public DateTime Date { get; set; }
 
-        /// <summary>
+    
         /// The activity ID for the entry. The ID value should map to an ID in the activities collection.
-        /// </summary>
         [Display(Name = "Activity")]
         public int ActivityId { get; set; }
 
-        /// <summary>
+
         /// The activity for the entry.
-        /// </summary>
         public Activity Activity { get; set; }
 
-        /// <summary>
+
         /// The duration for the entry (in minutes).
-        /// </summary>
         public double Duration { get; set; }
 
-        /// <summary>
+  
         /// The level of intensity for the entry.
-        /// </summary>
         public IntensityLevel Intensity { get; set; }
 
-        /// <summary>
-        /// Whether or not this entry should be excluded when calculating the total fitness activity.
-        /// </summary>
+     
+        /// Whether or not this entry should be excluded when calculating the total fitness activity.     
         public bool Exclude { get; set; }
 
-        /// <summary>
-        /// The notes for the entry.
-        /// </summary>
+        
+        /// The notes for the entry.   
+        [MaxLength(200, ErrorMessage = "The Notes field cannot be longer than 200 characters.")]
         public string Notes { get; set; }
     }
 }
